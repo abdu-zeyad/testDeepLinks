3012dea6d36e31a94f46f45cf2323444127710cc
abdalmajeed <abd.d@live.com>
2021-12-16 01:26:39 -0800

    add files

--- hooks/useCachedResources.ts
+++ hooks/useCachedResources.ts
@@ -2,9 +2,23 @@ import { FontAwesome } from '@expo/vector-icons';
 import * as Font from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 import * as React from 'react';
+import Constants from 'expo-constants';
+import * as Notifications from 'expo-notifications';
+import { Platform } from 'react-native';
 
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
 export default function useCachedResources() {
   const [isLoadingComplete, setLoadingComplete] = React.useState(false);
+  const notificationListener = React.useRef<Subscription>();
+  const responseListener = React.useRef<Subscription>();
+  const [expoPushToken, setExpoPushToken] = React.useState<string | undefined>();
 
   // Load any resources or data that we need prior to rendering the app
   React.useEffect(() => {
@@ -25,9 +39,79 @@ export default function useCachedResources() {
         SplashScreen.hideAsync();
       }
     }
+    registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
+
+    // This listener is fired whenever a user taps on or interacts with a notification (works when app is foregrounded, backgrounded, or killed)
+    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log(response);
+    });
+
 
     loadResourcesAndDataAsync();
+
+    return () => {
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
   }, []);
 
   return isLoadingComplete;
+}
+
+
+
+
+
+
+
+
+
+
+// import React, { useState, useEffect, useRef } from 'react';
+// import { Text, View, Button, Platform } from 'react-native';
+
+
+
+// export default function App() {
+//   const [expoPushToken, setExpoPushToken] = useState('');
+//   const [notification, setNotification] = useState(false);
+
+//   useEffect(() => {
+
+
+//   }, []);
+
+
+// }
+
+
+
+async function registerForPushNotificationsAsync() {
+  let token;
+  if (Constants.isDevice) {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+      alert('Failed to get push token for push notification!');
+      return;
+    }
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+    console.log(token);
+  } else {
+    alert('Must use physical device for Push Notifications');
+  }
+
+  if (Platform.OS === 'android') {
+    Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    });
+  }
+
+  return token;
 }
