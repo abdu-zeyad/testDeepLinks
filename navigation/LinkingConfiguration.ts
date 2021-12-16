b29b8e4dc2bc2ed5335f48c9148b7c2cacd04c76
abdalmajeed <abd.d@live.com>
2021-12-16 02:39:49 -0800

    add changes

--- navigation/LinkingConfiguration.ts
+++ navigation/LinkingConfiguration.ts
@@ -6,6 +6,7 @@
 
 import { LinkingOptions } from '@react-navigation/native';
 import * as Linking from 'expo-linking';
+import * as Notifications from 'expo-notifications';
 
 import { RootStackParamList } from '../types';
 
@@ -31,6 +32,40 @@ const linking: LinkingOptions<RootStackParamList> = {
       NotFound: '*',
     },
   },
+  // async getInitialURL() {
+  //   // First, you may want to do the default deep link handling
+  //   // Check if app was opened from a deep link
+  //   let url = await Linking.getInitialURL();
+  //   if (url != null) {
+  //     return url;
+  //   }
+  //   // Handle URL from expo push notifications
+  //   const response = await Notifications.getLastNotificationResponseAsync();
+  //   url = response?.notification.request.content.data.url;
+
+  //   return url;
+  // },
+  subscribe(listener) {
+    const onReceiveURL = ({ url }: { url: string }) => listener(url);
+
+    // Listen to incoming links from deep linking
+    Linking.addEventListener('url', onReceiveURL);
+
+    // Listen to expo push notifications
+    const subscription = Notifications.addNotificationResponseReceivedListener(response => {
+      const url = response.notification.request.content.data.url;
+      console.log(url, "url");
+
+      listener(url);
+    });
+
+    return () => {
+      // Clean up the event listeners
+      Linking.removeEventListener('url', onReceiveURL);
+      subscription.remove();
+    };
+  }
+
 };
 
 export default linking;
